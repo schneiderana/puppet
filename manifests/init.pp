3549e18d28245776a2b037ccaad019a0e7447dba
Vagrant Default User <vagrant@control>
2019-10-25 12:44:01 +0000

    download and unzip dokuwiki

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,40 @@
 # vim: set ts=2 sw=2 et:
 
 node server0 {
-# execute 'apt-get update'
+  # execute 'apt-get update'
   exec { 'apt-update':                    # exec resource named 'apt-update'
     command => '/usr/bin/apt-get update'  # command this resource will run
   }
 
-# install apache2 package
+  # install apache2 package
   package { 'apache2':
     require => Exec['apt-update'],        # require 'apt-update' before installing
     ensure => installed,
   }
 
-# ensure apache2 service is running
+  # ensure apache2 service is running
   service { 'apache2':
     ensure => running,
   }
 
-# install php7 package
-package { 'php7.3':
-  require => Exec['apt-update'],        # require 'apt-update' before installing
-  ensure => installed,
+  # install php7 package
+  package { 'php7.3':
+    require => Exec['apt-update'],        # require 'apt-update' before installing
+    ensure => installed,
+  }
+  
+  # download and install dokuwiki
+  exec { 'download_dokuwiki':
+    command => 'wget -O /usr/src/dokuwiki.tgz && https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+    provider => shell,
+    onlyif => 'test -f /usr/src/'
+  }
+  
+  # unzip dokuwiki
+  exec { 'unzip':
+    command => 'cd /usr/src && tar xavf dokuwiki.tgz && mv dokuwiki-2018-04-22b dokuwiki',
+    user => 'root',
+    require => Exec['download_dokuwiki'],
   }
 }
 #class apache{
